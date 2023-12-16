8dd46614974d89e1b8cdd6582b1649f84a8b4c1b
iamzwq <876867644@qq.com>
2023-12-16 12:16:20 +0800

    更新BigSpin组件

--- src/components/BigSpin/index.tsx
+++ src/components/BigSpin/index.tsx
@@ -1,9 +1,11 @@
-import { Spin } from "antd";
+import { FC } from "react";
+import { Spin, SpinProps } from "antd";
 import styles from "./index.module.css";
 
-const BigSpin = () => {
+const BigSpin: FC<SpinProps> = ({ ...props }) => {
   return (
     <Spin
+      {...props}
       indicator={
         <div className="relative w-[100px] h-[100px] m-a">
           <div className={styles.loaderBounce} />
